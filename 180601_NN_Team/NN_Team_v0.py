74870728ae5674f57cd5468940a70f1814605407
HERO-Kwon <herokwon@gmail.com>
2018-06-03 00:45:07 +0900

    running rnn

--- 180601_NN_Team/NN_Team_v0.py
+++ 180601_NN_Team/NN_Team_v0.py
@@ -1,7 +1,5 @@
 import numpy as np
 import tensorflow as tf
-from keras.models import Sequential
-from keras.layers import Dense
 import os
 from sklearn.model_selection import train_test_split
 
@@ -16,20 +14,79 @@ file_list = os.listdir(file_path)
 file_list_data = [os.path.join(file_path,s) for s in file_list if not "_idx.npy" in s]
 file_list_idx = [os.path.join(file_path,s) for s in file_list if "_idx.npy" in s]
 
-
-
 x_train,x_test,y_train,y_test = train_test_split(file_list_data,file_list_idx,test_size=0.2,random_state=33)
+data_train = (x_train,y_train)
+data_test = (x_test,y_test)
 
-with np.load()
+def gen_readnpy(files_data):
+    for i in range(len(files_data[0])):
+        data_read = np.load(files_data[0][i]).astype('float32')
+        label_read = np.load(files_data[1][i]).astype('int32')
 
+        yield data_read,label_read
 
+iter_train = gen_readnpy(data_train)
+iter_test = gen_readnpy(data_test)
+
+tf.reset_default_graph()
+## Network
+learning_rate = 0.001
+n_input = 6*30
+n_step  = 500
+n_class = 104
+total_batch = 6
+n_hidden = 128
+batch_size = 1000
+
+X = tf.placeholder(tf.float32,[None,n_step,n_input])
+Y = tf.placeholder(tf.int32,[None,n_class])
+
+W = tf.Variable(tf.random_normal([n_hidden,n_class]))
+b = tf.Variable(tf.random_normal([n_class]))
+
+cell = tf.nn.rnn_cell.BasicRNNCell(n_hidden)
+
+outputs, states = tf.nn.dynamic_rnn(cell,X,dtype=tf.float32)
+
+outputs = tf.transpose(outputs,[1,0,2])
+outputs = outputs[-1]
+
+model = tf.matmul(outputs,W) + b
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+    logits=model,labels=Y))
+
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+
+#tf.reset_default_graph()
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    total_cost = 0
+    for i in range(total_batch):
+        try:
+            batch_xs,batch_ys = next(iter_train)
+            batch_ys = np.eye(n_class)[batch_ys].squeeze(1)
+            batch_xs = batch_xs.reshape((batch_size,n_step,n_input))
+            _,cost_val = sess.run([optimizer,cost],
+            feed_dict = {X:batch_xs,Y:batch_ys})
+
+            total_cost += cost_val
+            print(cost_val)
+            
+            #value = sess.run(next_element)
+            #print(f"{value[1].shape}", end=" ")
+        except tf.errors.OutOfRangeError:
+            print("End of Dataset")
+        #print('Epoch:','%04d' % (epoch+1),
+        #'Avg.cost = ',':.3f'.format(total_cost / total_batch))
 
 
 ###
-
+'''
 def read_npy_file(filename_data,filename_idx):
-    data_read = np.load(filename_data.decode())
-    idx_read = np.load(filename_idx.decode())
+    data_read = np.float32(np.load(filename_data.decode()))
+    idx_read = np.float32(np.load(filename_idx.decode()))
     return data_read,idx_read
 
 def make_dataset(filenames_data,filenames_idx):
@@ -44,19 +101,66 @@ def make_dataset(filenames_data,filenames_idx):
 train_dataset = make_dataset(x_train,y_train)
 test_dataset = make_dataset(x_test,y_test)
 
+handle = tf.placeholder(tf.string, shape=[])
+iterator = tf.data.Iterator.from_string_handle(
+    handle, training_dataset.output_types, training_dataset.output_shapes)
+
+next_element = iterator_train.get_next()
 iterator_train = train_dataset.make_one_shot_iterator()
 iterator_test = test_dataset.make_one_shot_iterator()
-next_element = iterator_train.get_next()
+
+
+## Network
+learning_rate = 0.001
+n_input = 6*30
+n_step  = 500
+n_class = 100
+total_batch = 6
+n_hidden = 128
+batch_size = 1000
+
+X = tf.placeholder(tf.float32,[None,n_step,n_input])
+Y = tf.placeholder(tf.float32,[None,n_class])
+
+W = tf.Variable(tf.random_normal([n_hidden,n_class]))
+b = tf.Variable(tf.random_normal([n_class]))
+
+cell = tf.nn.rnn_cell.BasicRNNCell(n_hidden)
+
+outputs, states = tf.nn.dynamic_rnn(cell,X,dtype=tf.float32)
+
+outputs = tf.transpose(outputs,[1,0,2])
+outputs = outputs[-1]
+
+model = tf.matmul(outputs,W) + b
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+    logits=model,labels=Y))
+
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+
+#tf.reset_default_graph()
 
 with tf.Session() as sess:
-    for i in range(10):
+    training_handle = sess.run(iterator_train.string_handle())
+    sess.run(tf.global_variables_initializer())
+    total_cost = 0
+    for i in range(total_batch):
         try:
-            value = sess.run(next_element)
-            print(f"{value[1].shape}", end=" ")
+            batch_xs,batch_ys = training_handle
+            batch_xs = tf.reshape(batch_xs,(batch_size,n_step,n_input))
+            _,cost_val = sess.run([optimizer,cost],
+            feed_dict = {X:batch_xs,Y:batch_ys})
+
+            total_cost += cost_val
+    
+            
+            #value = sess.run(next_element)
+            #print(f"{value[1].shape}", end=" ")
         except tf.errors.OutOfRangeError:
             print("End of Dataset")
-
-
+        print('Epoch:','%04d' % (epoch+1),
+        'Avg.cost = ',':.3f'.format(total_cost / total_batch))
 
 ###
 
@@ -110,3 +214,4 @@ for person in image_raw.person.unique():
     data_train, data_test = train_test_split(image_raw[image_raw.person == person],test_size = 0.5)
     image_train = image_train.append(data_train)
     image_test = image_test.append(data_test)
+'''
