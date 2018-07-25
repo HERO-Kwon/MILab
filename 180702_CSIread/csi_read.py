83aaf9e6339284687a72bd5f58e0ed0c3c375301
HERO Kwon <herokwon@gmail.com>
2018-07-25 14:02:04 +0900

    csi_read : debug

--- 180702_CSIread/csi_read.py
+++ 180702_CSIread/csi_read.py
@@ -11,9 +11,10 @@ file_list = os.listdir(file_path)
 '''
 # For Linux
 dateid = 'csi201807231653'
-file_path = '/home/mint/Drv/HDD1TB/Data/CSI' + dateid
+file_path = '/home/mint/Drv/HDD1TB/Data/CSI/' + dateid
 file_list = os.listdir(file_path)
 
+print("CSI:Abs value")
 abs_csi = {}
 for file in file_list:
     data_read = sio.loadmat(os.path.join(file_path,file))
@@ -21,14 +22,15 @@ for file in file_list:
     csi_num = num_search.group(0)
     abs_csi[csi_num] = np.abs(data_read['csi'])
 
+print("CSI:to array")
 list_abs = []
 for i in range(len(abs_csi)):
     abs_val = np.mean(abs_csi[str(i+1)],axis=2)
     if abs_val.shape==(1,2):
         list_abs.append(abs_val)
-
 arr_abs = np.stack(list_abs, axis=-1)
 
+print("CSI: plot")
 for i in range(1):
     for j in range(2):
         plt.plot(arr_abs[i,j,:],label=str((i,j)))
