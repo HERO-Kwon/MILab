2394104287054367ef099c0bce9fc287fb449dd8
HERO Kwon <herokwon@gmail.com>
2019-04-27 16:23:38 +0900

    AdvP_A2: making pthread

--- 190412_AdvP_A2/dft_pthread/dft.cc
+++ 190412_AdvP_A2/dft_pthread/dft.cc
@@ -29,6 +29,7 @@ void dft1d(complex_t *h, complex_t *w, const unsigned N) {
     //         bit-reversed order.
     // Step 2: Follow Danielson-Lanczos Lemma to perform the DFT.
     /* Assignment */
+
     //block
     for(int r = 0; r < int(1024*1024/N) ; r++)
     {
@@ -36,13 +37,6 @@ void dft1d(complex_t *h, complex_t *w, const unsigned N) {
         //calc
         for(int k=0; k < int(N/2); k++)
         {
-            //dft_calc2(&h[n_start+k],&h[n_start+k + int(N/2)],w[k*int(1024/2)]);
-            //complex_t h1 = h[n_start+k];
-            //complex_t h2 = h[n_start+k + int(N/2)];
-            //complex_t w_val = w[k*int(1024/2)];
-            
-            //complex_t h1_new = h[n_start+k] + w[k*int(1024/2)] * h[n_start+k + int(N/2)];
-            //complex_t h2_new = h[n_start+k] - w[k*int(1024/2)] * h[n_start+k + int(N/2)];
             complex_t h1_new, h2_new, wval;
 
             int w_num = k*int(1024/N);
@@ -51,13 +45,9 @@ void dft1d(complex_t *h, complex_t *w, const unsigned N) {
 
             h1_new = h[n_start+k] + wval * h[n_start+k + int(N/2)];
             h2_new = h[n_start+k] - wval * h[n_start+k + int(N/2)];
-            
-            //h1_new = new complex_t *(h[n_start+k] + w[k*int(1024/2)] * h[n_start+k + int(N/2)]);
-            //h2_new = new complex_t *(h[n_start+k] - w[k*int(1024/2)] * h[n_start+k + int(N/2)])
 
             h[n_start+k] = h1_new;
             h[n_start+k + int(N/2)] = h2_new;
-
         }
     }
 
@@ -69,11 +59,27 @@ void* dft_thread(void *arg) {
     // Each thread performs 1-D DFT for height/num_threads number of rows.
     /* Assignment */
 
-    // beginning of critical section
-    // pthread_mutex_lock(&lock);
-    // use condition var to wait turn
-    
+    //cast and dereference the input argument.
+    unsigned tid = * (unsigned*) arg;
 
+    //beginning of the critical section
+    pthread_mutex_lock(&lock);
+    //use condition variable to wait for thread's turn.
+    while(tid > turn)
+    {
+        pthread_cond_wait(&cond, &lock);
+    }    
+
+    //accumulate thread ID values
+    sum += tid;
+    //update the turn
+    turn++;
+    //printf() is in the critical section only for demonstration purpose.
+    printf("thread %d: %u\n",tid,sum-tid,sum);
+
+    //wake up all other threads to check if they are the next one to go.
+    pthread_cond_broadcast(&cond);
+    pthread_mutex_unlock(&lock);
 
     return 0;
 }
@@ -100,114 +106,125 @@ void dft2d() {
     // Step 10: Deallocated heap memory blocks if any.
     /* Assignment */
 
-    // Step1: Pre-Calc Weight
-    cout << "Step1" << endl;
-    // Allocate data.
-    unsigned N = width; //width=1024
-
-    //make array
-    complex_t *w_arr = (complex_t*)malloc(N * sizeof(complex_t));
-    // Pre-Calc Weights.
-    for(unsigned r = 0; r < N/2-1; r++)
+    cout << "Step1-1: Pre-Calc Weight" << endl;
+    //make w array
+    complex_t *w_arr = (complex_t*)malloc(width * sizeof(complex_t));
+    
+    for(unsigned r = 0; r < width/2-1; r++) //Loop for N/2-1 times
     {
-        //make W value.
-        // W^n = cos(2n*pi/N) - jsin(2n*pi/N)
-        w_arr[r] = * new complex_t(cos(2*r*M_PI/N),-1*sin(2*r*M_PI/N));
+        w_arr[r] = * new complex_t(cos(2*r*M_PI/width),-1*sin(2*r*M_PI/width)); //make w value.
     }
+    
 
-    cout << "w[0].re:"<< w_arr[0].re << endl;
-    cout << "w[0].im:"<< w_arr[0].im << endl;
-    cout << "w[1].re:"<< w_arr[1].re << endl;
-    cout << "w[1].im:"<< w_arr[1].im << endl;
-
-    // Step1: Shuffle inputs to binary order
-    complex_t *old_data0 = data;
-    //data = (complex_t*)malloc(N*N * sizeof(complex_t));
+    cout << "dft1d-Step1: Shuffle inputs to binary order" << endl;
+    //make new data array
+    complex_t *old_data = data;
     data = new complex_t[width * height];
     
-    //make array
-    //complex_t *x_sh = (complex_t*)malloc(N*N * sizeof(complex_t));
-    // row loop
-    for(unsigned row=0; row<N; row++)
+    for(unsigned row=0; row<height; row++) // Do it for every row
     {
-        for(unsigned r=0; r<N; r++)
+        for(unsigned r=0; r<width; r++) // do it for every value
         {
             //calc shuffle order
             int quo_2 = r;
             int rem_2 = 0;
             int new_ind = 0;
-            for(int k = int(log2(N))-1; k>=0 ; k--)
+            for(int k = int(log2(width))-1; k>=0 ; k--)
             {
                 rem_2 = quo_2 % 2;
                 quo_2 = int(quo_2 / 2);
                 new_ind += pow(2,k) * rem_2;
             }
-            data[height*row + new_ind] = old_data0[height*row + r];
-            old_data0[height*row + r].~complex_t();
+            //put reordered data to array
+            data[height*row + new_ind] = old_data[height*row + r];
+            old_data[height*row + r].~complex_t();
         }
     }
-    delete[] old_data0;
-
-
-    // Save the result to a file.
-    file_handler.save_data("temp-shuffle.txt", data, width, height);
-    //Step2: initialize variables
-    //cout << "Step2" << endl;
+    delete[] old_data;
+        
+    cout << "Step2: Initialize thread variables" << endl;
+    unsigned sum = 0;
+    unsigned turn  = 0;
     pthread_mutex_t lock;
     pthread_cond_t cond;
+
+    // number of threads to create -> main
+    //initialize mutex lock and condition variable.
+    assert(!pthread_mutex_init(&lock,0));
+    assert(!pthread_cond_init(&cond,0));
+
+    //allocate pthreads.
+    pthread_t *threads = new pthread_t[num_threads];
+    unsigned *tid = new unsigned[num_threads];
+
+    //create pthreads and make each thread run func() above.
+    for(unsigned t=0; t<num_threads; t++)
+    {
+        tid[t] = t;
+        assert(!pthread_create(&threads[t],0,&dft_thread,&tid[t]));
+    }
+
+    //join pthreads/
+    for(unsigned t=0; t<num_threads; t++)
+    {
+        pthread_join(threads[t],0);
+        printf("thread %u done\n",t);
+    }
+
+    // deallocate pthreads.
+    delete [] threads;
+    delete [] tid;
+
+    // destroy mutex lock
+    assert(!pthread_mutex_destroy(&lock));
+    assert(!pthread_cond_destroy(&cond));
+
+    /*
     
     // Do it without thread
-    
     //dft calc
     for(int r=1; r<=10; r++)
     {
         dft1d(data,w_arr,pow(2,r));
     }
-    // Save the result to a file.
-    file_handler.save_data("temp-dft.txt", data, width, height);
-    
-    
     //transpose array
-
     complex_t *old_data1 = data;
     //data = (complex_t*)malloc(N*N * sizeof(complex_t));
     data = new complex_t[width * height];
     
     // Copy elements from the old array.
-    for(unsigned i = 0; i < N; i++)
+    for(unsigned i=0; i<height; i++)
     {
-        for(unsigned j=0;j<N;j++)
+        for(unsigned j=0; j<width; j++)
         {
-            new (&data[i*N+j]) complex_t(old_data1[i+j*N]);
-            old_data1[i+j*N].~complex_t();
+            new (&data[i*height+j]) complex_t(old_data1[i+j*width]);
+            old_data1[i+j*width].~complex_t();
         }
 
     }
     // Deallocate the old array.
     delete[] old_data1;
     
-    // Step1: Shuffle inputs to binary order
+    // Shuffle input to binary
+    //make new data array
     complex_t *old_data2 = data;
-    //data = (complex_t*)malloc(N*N * sizeof(complex_t));
     data = new complex_t[width * height];
     
-    //make array
-    //complex_t *x_sh = (complex_t*)malloc(N*N * sizeof(complex_t));
-    // row loop
-    for(unsigned row=0; row<N; row++)
+    for(unsigned row=0; row<height; row++) // Do it for every row
     {
-        for(unsigned r=0; r<N; r++)
+        for(unsigned r=0; r<width; r++) // do it for every value
         {
             //calc shuffle order
             int quo_2 = r;
             int rem_2 = 0;
             int new_ind = 0;
-            for(int k = int(log2(N))-1; k>=0 ; k--)
+            for(int k = int(log2(width))-1; k>=0 ; k--)
             {
                 rem_2 = quo_2 % 2;
                 quo_2 = int(quo_2 / 2);
                 new_ind += pow(2,k) * rem_2;
             }
+            //put reordered data to array
             data[height*row + new_ind] = old_data2[height*row + r];
             old_data2[height*row + r].~complex_t();
         }
@@ -221,28 +238,24 @@ void dft2d() {
     }
 
     // transpose again
-
     complex_t *old_data3 = data;
     //data = (complex_t*)malloc(N*N * sizeof(complex_t));
     data = new complex_t[width * height];
     
     // Copy elements from the old array.
-    for(unsigned i = 0; i < N; i++)
+    for(unsigned i=0; i<height; i++)
     {
-        for(unsigned j=0;j<N;j++)
+        for(unsigned j=0; j<width; j++)
         {
-            new (&data[i*N+j]) complex_t(old_data3[i+j*N]);
-            old_data3[i+j*N].~complex_t();
+            new (&data[i*height+j]) complex_t(old_data3[i+j*width]);
+            old_data3[i+j*width].~complex_t();
         }
 
     }
     // Deallocate the old array.
     delete[] old_data3;
 
-    // Save the result to a file.
-    file_handler.save_data("temp-dft2.txt", data, width, height);
-    
     //Step3: make thread
-    
+    */
 }
 
