a62cb8e8d95b6ac215653a34533da9ed054d7649
HERO Kwon <herokwon@gmail.com>
2019-05-12 21:31:16 +0900

    advp_a3: making 1d

--- 190509_AdvP_A3/dft_mpi/dft.cc
+++ 190509_AdvP_A3/dft_mpi/dft.cc
@@ -6,6 +6,10 @@
 
 using namespace std;
 
+//make global w array
+complex_t *w_arr = (complex_t*)malloc(512 * sizeof(complex_t));
+
+
 // Custom MPI_Allgather function with complex_t data type
 void mpi_allgather(complex_t *m_send_buffer, int m_send_count,
                    complex_t *m_recv_buffer, int m_recv_count,
@@ -17,6 +21,10 @@ void mpi_allgather(complex_t *m_send_buffer, int m_send_count,
     /* Assignment */
 
     // byte 단위로 보내고 계산해야 함
+    //MPI_Request mpi_recv_request;
+
+    //int size = sizeof(data) / num_ranks;
+    //MPI_Allgather(&rank_id,size,MPI_FLOAT,m_recv_buffer,size,MPI_FLOAT,MPI_COMM_WORLD);
     
 }
 
@@ -50,6 +58,65 @@ void dft1d(complex_t *h, const unsigned N) {
     // previous pthread assignment, or you may directly calculate the DFT
     // equation shown in the lecture note.
     /* Assignment */
+
+    // Step 1
+    //cout << "DFT:Step1: Shuffle inputs to binary order" << endl;
+    //make new data array
+    complex_t *dft_data;
+    dft_data = new complex_t[width * height/num_ranks];
+    
+    for(unsigned c=0; c<height/num_ranks; c++) // Do it for every row
+    {
+        for(unsigned r=0; r<width; r++) // do it for every value
+        {
+            // initialize variables
+            int quo_2 = r; 
+            int rem_2 = 0;
+            int new_ind = 0;
+            //calc binary shuffle order by dividing 2
+            for(int k = int(log2(width))-1; k>=0 ; k--)
+            {
+                rem_2 = quo_2 % 2;
+                quo_2 = int(quo_2 / 2);
+                new_ind += pow(2,k) * rem_2; //order number in decimal value
+            }
+            //put reordered data to array
+            new (&dft_data[height*c + new_ind]) complex_t(h[height*c + r]);
+        }
+    }
+    
+    // Step 2
+    //cout << "DFT:Step2: Perform DFT" << endl;
+    // iterate power of 2
+    for(int p=1; p<=int(log2(N)); p++)
+    {
+        int size = pow(2,p);
+        // do it for every row
+        for(int r = 0; r < int(width*(height/num_ranks)/size) ; r++)
+        {
+            // calculation of DFT value
+            for(int k=0; k < int(size/2); k++)
+            {
+                complex_t h1_new, h2_new, wval;
+
+                //get w index
+                int w_num = k*int(1024/size);
+                if(w_num >= 512) wval =  complex_t(-1.0,0.0) * w_arr[w_num-512];
+                else wval = w_arr[w_num];
+
+                //perform DFT
+                h1_new = dft_data[r*size+k] + wval * dft_data[r*size+k + int(size/2)];
+                h2_new = dft_data[r*size+k] - wval * dft_data[r*size+k + int(size/2)];
+
+                //put new value into data array
+                dft_data[r*size+k] = h1_new;
+                dft_data[r*size+k + int(size/2)] = h2_new;
+                h[r*size+k] = h1_new;
+                h[r*size+k + int(size/2)] = h2_new;
+            }
+        }
+    }
+
 }
 
 // Perform 2-D DFT.
@@ -64,5 +131,56 @@ void dft2d() {
     //         results calculated in the previous step.
     // Step 6: Transpose the matrix back to the original orientation.
     /* Assignment */
+
+
+    cout << "Step0: Pre-Calc Weight" << endl;
+    for(unsigned r = 0; r < unsigned(width/2-1); r++) //Loop for N/2-1 times
+    {
+        w_arr[r] = * new complex_t(cos(2*r*M_PI/width),-1*sin(2*r*M_PI/width)); //make w value.        
+    }
+
+    // print the number of ranks and rank ID
+    cout << "Temp: Number of ranks = " << num_ranks
+    << ", rank ID = " << rank_id << endl;
+
+    // temp
+    //unsigned buffer_size = num_ranks;
+    //int recv_buffer[buffer_size];
+
+    //MPI_Allgather(&data[0].re,1,MPI_FLOAT,recv_buffer,1,MPI_FLOAT,
+    //MPI_COMM_WORLD);
+
+    unsigned buffer_size = width*height;
+    complex_t *recv_buffer = (complex_t*)malloc(buffer_size * sizeof(complex_t));
+    //recv_buffer = new complex_t[buffer_size];
+    
+    printf("rank %d:\n",rank_id);
+    
+    int n_start = width*(height/num_ranks)*rank_id; //starting position of this data block
+
+    //data_mpi = data[n_start];
+    dft1d(&data[n_start],width); // calculate DFT
+
+    // the message is gathered across all process
+    //mpi_allgather(&rank_id,buffer_size,recv_buffer,buffer_size,MPI_COMM_WORLD);
+    //MPI_Allgather(&rank_id,1,MPI_INT,recv_buffer,1,MPI_INT,MPI_COMM_WOR LD);
+    MPI_Allgather(&data[n_start],width*(height/num_ranks)*sizeof(complex_t),MPI_BYTE,
+    recv_buffer,width*(height/num_ranks)*sizeof(complex_t),MPI_BYTE,MPI_COMM_WORLD);
+
+
+    //transpose_matrix(data,width,height);
+
+    //MPI_Allgather(data,sizeof(complex_t),MPI_BYTE,recv_buffer,sizeof(complex_t),MPI_BYTE,MPI_COMM_WORLD);
+    
+
+
+    // print the information of gathered message.
+    cout << "MPI Rank " << rank_id << " has the array of [";
+    //for(unsigned i=0; i<buffer_size; i++){
+        cout << recv_buffer[rank_id*width*(height/num_ranks)].re << " " ;
+        cout << recv_buffer[rank_id*width*(height/num_ranks)].im << " " ;
+        cout << "\b]" << endl;
+    //}
+    //data = recv_buffer;
 }
 
