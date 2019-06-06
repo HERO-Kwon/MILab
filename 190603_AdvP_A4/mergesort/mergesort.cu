6e850fa58ce4d6f52c0c907f9163bcae6ac0cccf
HERO Kwon <herokwon@gmail.com>
2019-06-06 15:46:47 +0900

    advp_a4: cuda multiply

--- 190603_AdvP_A4/mergesort/mergesort.cu
+++ 190603_AdvP_A4/mergesort/mergesort.cu
@@ -1,39 +1,70 @@
 #include "mergesort.h"
 
+using namespace std;
+
+// Device kernel function
+__global__ void vector_mul(int *a_dev, int *b_dev, int *c_dev){
+    //Element-wise vector multiplication
+    unsigned idx = blockIdx.x * blockDim.x + threadIdx.x;
+    c_dev[idx] = a_dev[idx] * b_dev[idx];
+}
+
 void mergesort(unsigned *m_data) {
     /* Assignment */
-    void partition(int left,int right)
-    {
-	int mid;
-	if (left < right)
-        {
-            mid = (left + right) / 2; 
-            partition(left, mid);
-            partition(mid + 1, right);
-            merge(left, right);
-        }
+
+    // get vector vector_size
+    unsigned vector_size = num_data;
+
+
+    // Host code
+    int *a = 0, *b = 0, *c = 0;
+    int *a_dev = 0, *b_dev = 0, *c_dev = 0;
+
+    // Host data
+    a = (int*)malloc(vector_size*sizeof(int));
+    b = (int*)malloc(vector_size*sizeof(int));
+    c = (int*)malloc(vector_size*sizeof(int));
+
+    for(unsigned i = 0; i < vector_size; i++){
+        a[i] = m_data[i];
+        b[i] = m_data[i];
     }
+    memset(c,0,vector_size*sizeof(int));
 
-void merge(int left, int right)
-    {
-        int mid = (left + right) / 2;
+    // Device memory allocation
+    cudaMalloc(&a_dev, vector_size * sizeof(int));
+    cudaMalloc(&b_dev, vector_size * sizeof(int));
+    cudaMalloc(&c_dev, vector_size * sizeof(int));
 
-        int i = left;
-        int j = mid + 1;
-        int k = left;
-        while (i <= mid && j <= right)
-        {
-            if (arr[i] <= arr[j]) 
-                arr2[k++] = arr[i++]; 
-            else
-                arr2[k++] = arr[j++];
-        }
+    // Memory copy from host to device memory
+    cudaMemcpy(a_dev, a, vector_size * sizeof(int), cudaMemcpyHostToDeivce);
+    cudaMemcpy(b_dev, b, vector_size * sizeof(int), cudaMemcpyHostToDeivce);
 
-        int tmp = i>mid ? j : i;
-        
-        while(k<=right) arr2[k++] = arr[tmp++];
+    // Kernel launch
+    vector_mul<<<vector_size/block_size,block_size>>>(a_dev,b_dev,c_dev);
 
-        for (int i=left;i<=right;i++) arr[i] = arr2[i];
-    }
+    // memory copy from device to host memory
+    cudaMemcpy(c,c_dev,vector_size * sizeof(int), cudaMemcpyDeviceToHost);
+
+    // print vector add results
+    cout << "a = [";
+    for(unsigned i=0; i<10; i++) {cout << a[i] << " "; }
+    cout << "\b]" << endl;
+    cout << "*" << endl;
+    cout << "b = [";
+    for(unsigned i=0; i<10; i++) {cout << b[i] << " "; }
+    cout << "\b]" << endl;
+    cout << "=" << endl;
+    cout << "c = [";
+    for(unsigned i=0; i<10; i++) {cout << c[i] << " "; }
+    cout << "\b]" << endl;
+    
+
+    // Host memory deallocation
+    free(a);free(b);free(c);
+
+    // Device memory deallocation
+    cudaFree(a_dev); cudaFree(b_dev); cudaFree(c_dev);
+
 }
 
