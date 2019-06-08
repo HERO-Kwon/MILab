9d565b0521dcee75f9038994f1ee862e1612a7e8
HERO Kwon <herokwon@gmail.com>
2019-06-09 01:10:43 +0900

    advp_a4: add comments

--- 190603_AdvP_A4/mergesort/mergesort.cu
+++ 190603_AdvP_A4/mergesort/mergesort.cu
@@ -4,133 +4,101 @@
 #include "mergesort.h"
 
 // Device kernel function
-__global__ void vector_mul(int *a_dev, int *b_dev, int *c_dev){
-    //Element-wise vector multiplication
-    unsigned idx = blockIdx.x * blockDim.x + threadIdx.x;
-    c_dev[idx] = a_dev[idx] + b_dev[idx];
-}
-//
-// Finally, sort something
-// gets called by gpu_mergesort() for each slice
-//
-__device__ void gpu_bottomUpMerge(int* source, int* dest, int start, int middle, int end) {
-    int i = start;
+__device__ void Merge(int* source, int* dest, int start, int middle, int end) {
+    int i = start; 
     int j = middle;
+    // iterate through points of target vector
     for (int k = start; k < end; k++) {
+        // if left part of target vector is smaller than right
         if (i < middle && (j >= end || source[i] < source[j])) {
             dest[k] = source[i];
             i++;
+        // if right part of target vector is larger than left
         } else {
             dest[k] = source[j];
             j++;
         }
     }
 }
-// Perform a full mergesort on our section of the data.
-//
-__global__ void gpu_mergesort(int* source, int* dest, int size, int width, int slices) {
+// setting values for target vector
+__global__ void SetMerge(int* source, int* dest, int size, int width, int slices) {
+    // make 1d index
     unsigned idx = blockIdx.x * blockDim.x + threadIdx.x;
-    int start = width*idx*slices, 
-         middle, 
-         end;
+    int start = width*idx*slices;
+    int middle; 
+    int end;
 
+    // iterate through all slices of target vectors
     for (int slice = 0; slice < slices; slice++) {
         if (start >= size)
             break;
 
         middle = min(start + (width / 2), size);
         end = min(start + width, size);
-        gpu_bottomUpMerge(source, dest, start, middle, end);
+        // call merge function
+        GpuMerge(source, dest, start, middle, end);
+        // set starting points for next target vector
         start += width;
     }
 }
-
-
 
 void mergesort(unsigned *m_data) {
     /* Assignment */
 
     using namespace std;   
+
+    // set block size
+    int blocksPerGrid = 8;
+
     // get vector vector_size
     unsigned vector_size = num_data;
 
-
     // Host code
-    int *a = 0, *b = 0, *c = 0;
-    int *a_dev = 0, *b_dev = 0, *c_dev = 0;
+    int *source_dev = 0, *dest_dev = 0;
 
     // Host data
-    a = (int*)malloc(vector_size*sizeof(int));
-    b = (int*)malloc(vector_size*sizeof(int));
-    c = (int*)malloc(vector_size*sizeof(int));
+    source_vec = (int*)malloc(vector_size*sizeof(int));
+    dest_vec = (int*)malloc(vector_size*sizeof(int));
 
     for(unsigned i = 0; i < vector_size; i++){
-        a[i] = m_data[i];
-        b[i] = m_data[i];
+        source_vec[i] = m_data[i];
     }
-    memset(c,0,vector_size*sizeof(int));
+    memset(dest_vec,0,vector_size*sizeof(int));
 
     // Device memory allocation
-    cudaMalloc(&a_dev, vector_size * sizeof(int));
-    cudaMalloc(&b_dev, vector_size * sizeof(int));
-    cudaMalloc(&c_dev, vector_size * sizeof(int));
+    cudaMalloc(&source_dev, vector_size * sizeof(int));
+    cudaMalloc(&dest_dev, vector_size * sizeof(int));
 
     // Memory copy from host to device memory
-    cudaMemcpy(a_dev, a, vector_size * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(b_dev, b, vector_size * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(source_dev, source_vec, vector_size * sizeof(int), cudaMemcpyHostToDevice);
 
-    // Kernel launch
-    //vector_mul<<<vector_size/block_size,block_size>>>(a_dev,b_dev,c_dev);
-
-    // Actually call the kernel
-    //gpu_mergesort<<<blocksPerGrid, threadsPerBlock>>>(A, B, size, width, slices, D_threads, D_blocks);
-
-    int blocksPerGrid = 8;
+    // make mergesort
     for (int width = 2; width < num_data*2; width *= 2) {
         int slices = num_data / (blocksPerGrid*block_size * width)+1;
 
-        // Actually call the kernel
-        gpu_mergesort<<<blocksPerGrid, block_size>>>(a_dev, c_dev, num_data, width, slices);
+        // call the kernel
+        gpu_mergesort<<<blocksPerGrid, block_size>>>(source_dev, dest_dev, num_data, width, slices);
 
-        // memory copy from device to host memory
+        // swap values of dest vector to source vector
         cudaMemcpy(c,c_dev,vector_size * sizeof(int), cudaMemcpyDeviceToHost);
 
-        cout << "c = [";
-	for(unsigned i=0;i<10;i++) {cout << c[i] << "\b]" << endl;}
+        // copy dest values to source
         for(unsigned i = 0; i < vector_size; i++){
-            a[i] = c[i];
+            source_vec[i] = dest_vec[i];
         }
-        cudaMemcpy(a_dev, a, vector_size * sizeof(int), cudaMemcpyHostToDevice);
+        // copy to device memory
+        cudaMemcpy(source_dev, source_vec, vector_size * sizeof(int), cudaMemcpyHostToDevice);
     }
-
-    // memory copy from device to host memory
-    cudaMemcpy(c,c_dev,vector_size * sizeof(int), cudaMemcpyDeviceToHost);
 
     for(unsigned i=0; i< vector_size; i++){
-        data[i] = c[i];
+        data[i] = dest_vec[i];
     }
     
-    // print vector add results
-    cout << "a = [";
-    for(unsigned i=0; i<10; i++) {cout << a[i] << " "; }
-    cout << "\b]" << endl;
-    cout << "*" << endl;
-    cout << "b = [";
-    for(unsigned i=0; i<10; i++) {cout << b[i] << " "; }
-    cout << "\b]" << endl;
-    cout << "=" << endl;
-    cout << "c = [";
-    for(unsigned i=0; i<10; i++) {cout << c[i] << " "; }
-    cout << "\b]" << endl;
-    
-
-
-
     // Host memory deallocation
-    free(a);free(b);free(c);
+    free(source_vec);free(dest_vec);
 
     // Device memory deallocation
-    cudaFree(a_dev); cudaFree(b_dev); cudaFree(c_dev);
+    cudaFree(source_dev); cudaFree(dest_dev);
 
 }
 
