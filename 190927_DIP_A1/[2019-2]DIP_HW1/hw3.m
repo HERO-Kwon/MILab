498d93e6ebbdeb72ca39819788e9661a131131a0
HERO Kwon <herokwon@gmail.com>
2019-09-27 17:00:43 +0900

    dip_a1_v1

--- 190927_DIP_A1/[2019-2]DIP_HW1/hw3.m
+++ 190927_DIP_A1/[2019-2]DIP_HW1/hw3.m
@@ -22,31 +22,34 @@ L = 256;
 hist = imhist(im,L);
 hist_match = imhist(im_match,L);
 
-% calculate pdf,cdf
+% calculate pdf
 pdf = hist ./ MN;
 pdf_match = hist_match ./ MN;
-
+% calculate transformation function
 s_k = round((L-1).*cumsum(pdf));
 G_zq = round((L-1).*cumsum(pdf_match));
 
 % map input image to histogram-equalized image
 output = zeros(size(im));
 for i = 1:L
+    % select from input image
     mask = (im >= (i-1)/L) & (im <= i/L);
+    % apply 1st transfer function
     T_rk = s_k(i);
-    
+    % find closest distance from G(zq)
     dist = abs(G_zq-T_rk);
     [val_dist,idx_dist] = min(dist);
-    
+    % apply 2nd transfer function
     T_sk = idx_dist;
+    % merge output
     output = output + mask .* (T_sk/L);
 end
 
-% fig 2: histogram of the output image
+% fig 3: histogram of the output image
 figure(3);
 imhist(output);
 title(['3-3-1. Histogram of Output Image']);
-% fig 3: Output image
+% fig 4: Output image
 figure(4);
 imshow(output);
 title(['3-3-2. Output Image']);
